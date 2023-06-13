342b885e483993fed75bf2a474748afae8b6a069
nGhart <107068059+nGhart@users.noreply.github.com>
2023-06-13 03:55:49 +0000

    Update contactSlice.js

--- src/slice/contactSlice.js
+++ src/slice/contactSlice.js
@@ -1,18 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    contacts: [],
-}
+  contacts: [],
+};
 const contactSlice = createSlice({
-    name: "contacts",
-    initialState,
-    reducers: {
-        addContact: (state, action) => {
-            state.contacts = [...state.contacts, action.payload]
-        },
-        deleteContact: () => { },
-        editContact: () => { }
-    }
-})
-export const {addContact, deleteContact, editContact}=contactSlice.actions;
+  name: 'contacts',
+  initialState,
+  reducers: {
+    addContact: (state, action) => {
+      state.contacts = [...state.contacts, action.payload];
+    },
+    deleteContact: (state, action) => {
+      state.contacts = state.contacts.filter((item) => {
+        if (item.id !== action.payload) {
+          return item;
+        }
+      });
+    },
+    editContact: (state, action) => {
+      state.contacts = state.contacts.map((item) => {
+        if (item.id === action.payload.id) {
+          return action.payload.newPerson;
+        }
+        return item;
+      });
+    },
+  },
+});
+export const { addContact, deleteContact, editContact } = contactSlice.actions;
 export default contactSlice.reducer;

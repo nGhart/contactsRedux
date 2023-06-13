ed4f5ec8ff0399ebb3295efe2bd5d39398e6fe83
nGhart <107068059+nGhart@users.noreply.github.com>
2023-06-13 03:56:27 +0000

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
-import appReducer from '../slice/contactSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import appReducer from '../slice/contactSlice';
 
 export const store = configureStore({
-    reducer: {
-userReducer: appReducer,
-    }
-})
+  reducer: {
+    userReducer: appReducer,
+  },
+});
